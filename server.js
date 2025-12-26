342ad70d3ef024ea2dc3ece3d4d8dd4dc0f1c60b
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-26 11:33:33 +0100

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 const DB_FILE = "./users.json";
