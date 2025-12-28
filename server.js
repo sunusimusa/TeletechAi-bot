75e85df900678ec06859626f2aaa17b040174103
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-28 23:49:46 +0100

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+require('dotenv').config()
 const axios = require("axios");
 
 const app = express();
