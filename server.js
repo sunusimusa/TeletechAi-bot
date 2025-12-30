5d7fe9b75158bbb6aab52523679374a8b5a8bcc5
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-30 18:51:27 +0100

    server.js

--- server.js
+++ server.js
@@ -32,6 +32,18 @@ const SPIN_REWARDS = [
 { label: "Nothing", reward: 0 }
 ];
 
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+  name: String,
+  leader: String,
+  members: [String],
+  totalScore: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Team", teamSchema);
+
 // ================= DATABASE =================
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("✅ MongoDB Connected"))
