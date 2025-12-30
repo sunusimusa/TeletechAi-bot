878650a85240c4770b3dd959a544b417daa7cc10
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-30 18:11:19 +0100

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 
-const { sendJetton } = require("./ton");
 const User = require("./models/User");
 
 const TOKEN_CONTRACT = process.env.TOKEN_CONTRACT;
