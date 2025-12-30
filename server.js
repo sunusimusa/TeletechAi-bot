95931f468dfa3b39d39e74a1920aa206cf7e8c39
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-30 16:59:47 +0100

    server.js

--- server.js
+++ server.js
@@ -74,8 +74,6 @@ mongoose.connect(process.env.MONGODB_URI)
 
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
-
 const teamSchema = new mongoose.Schema({
   name: String,
   leader: String,
@@ -83,8 +81,6 @@ const teamSchema = new mongoose.Schema({
   totalScore: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
-
-const Team = mongoose.model("Team", teamSchema);
 
 const User = require("./models/User");
 
