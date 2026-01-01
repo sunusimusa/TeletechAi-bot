db825a6cccc17773393da1c3093bacaba36ccfa4
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-01 11:27:17 +0100

    User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  userId: String,
+
+  balance: { type: Number, default: 0 },
+  energy: { type: Number, default: 100 },
+
+  lastEnergyUpdate: { type: Number, default: Date.now }
+});
+
+module.exports = mongoose.model("User", userSchema);
