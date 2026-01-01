fd8fea7a270e0d3d860781e7917920e0e471c32b
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-01 18:23:24 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   telegramId: { type: String, unique: true },
-
   balance: { type: Number, default: 0 },
   energy: { type: Number, default: 100 },
   freeTries: { type: Number, default: 3 },
   tokens: { type: Number, default: 0 },
-
   lastEnergy: { type: Number, default: Date.now },
-  lastDaily: { type: Number, default: 0 } // 👈 DAILY BONUS
+  lastDaily: { type: Number, default: 0 }
 });
 
 export default mongoose.model("User", userSchema);
