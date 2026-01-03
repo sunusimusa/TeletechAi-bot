541a61eddb5bcde4f9b4429e335ee7d512f5e2fd
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-03 16:25:33 +0100

    jetton.js

--- blockchain/jetton.js
+++ blockchain/jetton.js
@@ -1,1 +1,8 @@
+import { tonweb, wallet, keyPair } from "./ton.js";
 
+const jettonMinter = new tonweb.token.jetton.JettonMinter(
+  tonweb.provider,
+  { address: process.env.JETTON_MASTER }
+);
+
+export { jettonMinter };
