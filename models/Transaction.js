eb1a2c7a8bbe186e37deeaa0b43c9083420fef20
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-05 13:11:31 +0100

    Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,24 +1,36 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  from: String,           // sender wallet
-  to: String,             // receiver wallet
-  amount: Number,
-  gasFee: Number,
-  type: { type: String, enum: ["SEND", "RECEIVE"] },
-  createdAt: { type: Date, default: Date.now }
-});
-
 const TransactionSchema = new mongoose.Schema({
-  fromWallet: String,
-  toWallet: String,
-  amount: Number,
-  gasFee: { type: Number, default: 0 },
+  fromWallet: {
+    type: String,
+    required: true
+  },
+
+  toWallet: {
+    type: String,
+    required: true
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  gasFee: {
+    type: Number,
+    default: 0
+  },
+
   type: {
     type: String,
     enum: ["SEND", "RECEIVE", "BURN", "PRO_UPGRADE"],
+    required: true
   },
-  createdAt: { type: Date, default: Date.now }
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-export default mongoose.model("Transaction", transactionSchema);
+export default mongoose.model("Transaction", TransactionSchema);
