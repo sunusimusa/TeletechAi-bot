087f49b31084c05fda5f5f3fc1d223e217219f86
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-05 21:22:19 +0100

    season.js

---
+++ config/season.js
@@ -0,0 +1,13 @@
+export const REF_SEASON = {
+  name: "Season 1",
+  start: new Date("2026-01-01"),
+  end: new Date("2026-01-14"),
+  paid: false // ❗ very important
+};
+
+export const REF_REWARDS = {
+  1: 500,   // 🥇
+  2: 300,   // 🥈
+  3: 200,   // 🥉
+  rest: 50  // top 4–10
+};
