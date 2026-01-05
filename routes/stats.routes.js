3e4e11dd8fc0dcbd66445d682184c30b284044f7
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-05 17:04:18 +0100

    stats.routes.js

--- routes/stats.routes.js
+++ routes/stats.routes.js
@@ -31,23 +31,4 @@ router.get("/supply", async (req, res) => {
   });
 });
 
-async function loadSupply() {
-  const res = await fetch("/api/stats/supply");
-  const data = await res.json();
-
-  document.getElementById("totalSupply").innerText =
-    data.totalSupply.toLocaleString();
-
-  document.getElementById("burned").innerText =
-    data.burned.toLocaleString();
-
-  document.getElementById("systemBal").innerText =
-    data.systemBalance.toLocaleString();
-
-  document.getElementById("circulating").innerText =
-    data.circulating.toLocaleString();
-}
-
-loadSupply();
-
 export default router;
