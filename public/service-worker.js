ca8557da63038a467934ae9c26ab5b4e823a5294
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-11 07:55:30 +0100

    service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,22 @@
 const CACHE_NAME = "luckybox-v1";
-
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/sounds/click.mp3",
-  "/sounds/win.mp3",
-  "/sounds/lose.mp3"
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+self.addEventListener("install", e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
